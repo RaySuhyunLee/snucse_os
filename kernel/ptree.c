e1019bc4e599b52cb47302166b0d99889193f0a1
RaySuhyunLee <raysuhyunlee@gmail.com>
2017-03-14 16:52:09 +0900

    Refactor for cleaner parameters

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -27,38 +27,51 @@ how we define tasklist? is it already defined?
 
 //void read_lock(struct task_struct * p){ task_lock(p);}
 //void read_unlock(struct task_struct *p) { task_unlock(p);}
+struct SearchResult {
+	struct prinfo *data;
+	int max_size;
+	int count;
+}
 
 int ptree(struct prinfo * buf, int *nr) { // buf = point of proc. data,  nr = # of prinfo entries
 	if( buf == null || nr == null) return EINVAL;
 // EFAULT : if buf or nr are outside the accessible address space.
 
   struct prinfo *_buf;
-	int *_nr;
+	int num_to_read;
 
-	memcpy(_buf, buf, sizeof(struct prinfo));
-	memcpy(_nr, nr, sizeof(int));
+	memcpy(&num_to_read, nr, sizeof(int));
 
+	// initialize variables for traversal
 	pid_t init_pid = 1;
 	process_count = 0;
-	struct prinfo search_result[*_nr];
+	struct SearchResult result;
+	struct prinfo data[num_to_read];
+	result.data = &data;
+	result.max_size = num_to_read;
+	result.count = 0;
 	
-	// lock untii traversal to prevent data structures from changing
+	// lock untii traversal completes, to prevent data structures from changing
 	read_lock(&tasklist_lock);
-	preorder_search(init_pid);
+	preorder_search(init_pid, &process_count);
 	read_unlock(&tasklist_lock);
 
-//return # of entries on success
+	memcpy(nr, &result->count, sizeof(int));
+	memcpy(buf, result->data, (result->count) * sizeof(struct prinfo));
+
+	return process_count;
 }
+
 
 /*
  * recursively search for processes in preorder search.
  */
-
-void search_process_preorder(pid_t pid, int* count) {
+void search_process_preorder(pid_t pid, int* count, struct SearchResult *result) {
 	struct task_struct* task = find_task_by_vpid(pid);
-  push_task(task, parray, max);
+  push_task(task, result);
 	*count++;
 
+	// recursively search for every child
 	list_head* child_list = &task->children;
 	while(child_list != NULL) {
 		struct task_struct* child = container_of(child_list, struct task_struct, children);
@@ -67,18 +80,19 @@ void search_process_preorder(pid_t pid, int* count) {
 	}
 }
 
-void push_task(struct *prinfo search_result, struct task_struct* task) {
-	if (count > max)
+void push_task(struct task_struct* task, struct SearchResult* result) {
+	if (result->count >= max) break;
 
+	struct prinfo* data = result->data;
 	struct task_struct* child = container_of(&(task->children), struct task_struct, children);
 	struct task_struct* sibling = container_of(&(task->sibling), struct task_struct, sibling);
-	search_result[count].state = task->state;
-	search_result[count].pid = task->pid;
-	search_result[count].parent_pid = task->parent->pid;
-	search_result[count].first_child_pid = child->pid;
-	search_result[count].next_sibling_pid = sibling->pid;
-	search_result[count].uid = task->cred->pid;
+	data[count].state = task->state;
+	data[count].pid = task->pid;
+	data[count].parent_pid = task->parent->pid;
+	data[count].first_child_pid = child->pid;
+	data[count].next_sibling_pid = sibling->pid;
+	data[count].uid = task->cred->pid;
 	strcpy(temp.comm, task->comm);
 
-	count++;
+	result->count++;
 }
