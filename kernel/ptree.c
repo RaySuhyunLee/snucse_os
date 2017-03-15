6f0d0d7abdf5c62f7d75fab2113e0dd65207d029
RaySuhyunLee <raysuhyunlee@gmail.com>
2017-03-15 16:24:04 +0900

    Fix bugs

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,6 +1,8 @@
 #include <linux/prinfo.h>
 #include <uapi/asm-generic/errno-base.h>
 #include <linux/sched.h>
+#include <linux/types.h>
+#include <linux/slab.h>
 
 //depth-first search
 
@@ -27,37 +29,42 @@ how we define tasklist? is it already defined?
 
 //void read_lock(struct task_struct * p){ task_lock(p);}
 //void read_unlock(struct task_struct *p) { task_unlock(p);}
+//
 struct SearchResult {
 	struct prinfo *data;
 	int max_size;
 	int count;
-}
+};
 
-int ptree(struct prinfo * buf, int *nr) { // buf = point of proc. data,  nr = # of prinfo entries
-	if( buf == null || nr == null) return EINVAL;
-// EFAULT : if buf or nr are outside the accessible address space.
+void search_process_preorder(pid_t, int*, struct SearchResult*);
+void push_task(struct task_struct*, struct SearchResult*);
 
-  struct prinfo *_buf;
+int sys_ptree(struct prinfo * buf, int *nr) { // buf = point of proc. data,  nr = # of prinfo entries
+	
 	int num_to_read;
+	int process_count = 0;
+	struct SearchResult result;
+	struct prinfo *data = kmalloc(sizeof(struct prinfo)*num_to_read, GFP_KERNEL);
+	pid_t init_pid = 1;
+
+	if( buf == NULL || nr == NULL) return EINVAL;
+	// EFAULT : if buf or nr are outside the accessible address space.
 
 	memcpy(&num_to_read, nr, sizeof(int));
 
 	// initialize variables for traversal
-	pid_t init_pid = 1;
-	process_count = 0;
-	struct SearchResult result;
-	struct prinfo data[num_to_read];
-	result.data = &data;
+	if (!data) return -1; // FIXME proper error handling
+	result.data = data;
 	result.max_size = num_to_read;
 	result.count = 0;
 	
 	// lock untii traversal completes, to prevent data structures from changing
 	read_lock(&tasklist_lock);
-	preorder_search(init_pid, &process_count);
+	search_process_preorder(init_pid, &process_count, &result);
 	read_unlock(&tasklist_lock);
 
-	memcpy(nr, &result->count, sizeof(int));
-	memcpy(buf, result->data, (result->count) * sizeof(struct prinfo));
+	memcpy(nr, &(result.count), sizeof(int));
+	memcpy(buf, result.data, (result.count) * sizeof(struct prinfo));
 
 	return process_count;
 }
@@ -68,31 +75,40 @@ int ptree(struct prinfo * buf, int *nr) { // buf = point of proc. data,  nr = # 
  */
 void search_process_preorder(pid_t pid, int* count, struct SearchResult *result) {
 	struct task_struct* task = find_task_by_vpid(pid);
+	struct task_struct* child;
+	struct list_head* child_list;
   push_task(task, result);
-	*count++;
+	(*count)++;
 
 	// recursively search for every child
-	list_head* child_list = &task->children;
+	child_list = &(task->children);
 	while(child_list != NULL) {
-		struct task_struct* child = container_of(child_list, struct task_struct, children);
-		search_process_preorder(child->pid, parray, nr, count);
+		child = container_of(child_list, struct task_struct, children);
+		search_process_preorder(child->pid, count, result);
 		child_list = child_list->next;
 	}
 }
 
 void push_task(struct task_struct* task, struct SearchResult* result) {
-	if (result->count >= max) break;
+	int count = result->count;
 
-	struct prinfo* data = result->data;
-	struct task_struct* child = container_of(&(task->children), struct task_struct, children);
-	struct task_struct* sibling = container_of(&(task->sibling), struct task_struct, sibling);
+	struct prinfo* data;
+	struct task_struct* child;
+	struct task_struct* sibling;
+
+	if (result->count >= result->max_size) return;
+
+	data = result->data;
+	child = container_of(&(task->children), struct task_struct, children);
+	sibling = container_of(&(task->sibling), struct task_struct, sibling);
+
 	data[count].state = task->state;
 	data[count].pid = task->pid;
 	data[count].parent_pid = task->parent->pid;
 	data[count].first_child_pid = child->pid;
 	data[count].next_sibling_pid = sibling->pid;
-	data[count].uid = task->cred->pid;
-	strcpy(temp.comm, task->comm);
+	data[count].uid = task->cred->uid;
+	strcpy(data[count].comm, task->comm);
 
 	result->count++;
 }
