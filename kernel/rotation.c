59cc17655d4b10e9ebdaec86925d7847fed73266
RaySuhyunLee <raysuhyunlee@gmail.com>
2017-03-30 03:39:35 +0900

    Implement degree variable and following operations

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -1,11 +1,17 @@
 #include <linux/rotation.h>
 #include <linux/types.h>
-#include <linux/sched.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
+#include <linux/spinlock.h>
+#include <linux/spinlock_types.h>
+
+int _degree;	// current degree
+DEFINE_SPINLOCK(degree_lock);
 
 int sys_set_rotation(int degree) {
-	printk(KERN_DEBUG "set_rotation");
+	spin_lock(&degree_lock);
+	_degree = degree;
+	spin_unlock(&degree_lock);
+
+	printk(KERN_DEBUG "set_rotation to %d", _degree);
 	return 1;
 }
 
