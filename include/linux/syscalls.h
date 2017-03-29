b8ab9a4c7cf587226c380db6c9cea21a6b633848
JaeDong Hwang <jdhwang730@gmail.com>
2017-03-29 20:54:51 +0900

    implementing set_rotation

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -846,4 +846,11 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 			 unsigned long idx1, unsigned long idx2);
 asmlinkage long sys_finit_module(int fd, const char __user *uargs, int flags);
+
+asmlinkage int set_rotation(int degree);
+//asmlinkage int rotlock_read (int degree, int range);
+//asmlinkage int rotlock_write(int degree, int range);
+//asmlinkage int rotunlock_read(int degree, int range);
+//asmlinkage int rotunlock_write(int degree, int range);
+
 #endif
---
+++ test/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main () {
+	int c;
+	c = syscall(380,3);
+	c = syscall(381,1,2);
+	c= syscall (382,1,2);
+	c= syscall (383,1,2);
+	c= syscall (384,1,2);
+	return true;
+}
+
