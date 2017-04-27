3a6598dc455b7841d13c706525b5d27b4093998a
JaeDong Hwang <jdhwang730@gmail.com>
2017-04-27 22:59:02 +0900

    change default policy

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -168,7 +168,7 @@ extern struct task_group root_task_group;
 	.prio		= MAX_PRIO-20,					\
 	.static_prio	= MAX_PRIO-20,					\
 	.normal_prio	= MAX_PRIO-20,					\
-	.policy		= SCHED_NORMAL,					\
+	.policy		= SCHED_WRR,					\
 	.cpus_allowed	= CPU_MASK_ALL,					\
 	.nr_cpus_allowed= NR_CPUS,					\
 	.mm		= NULL,						\
