2a6af534fb2347b4075822899e64ea6f566e7a1d
JaeDong Hwang <jdhwang730@gmail.com>
2017-04-30 11:12:27 +0900

    modify rt_sched_class.next

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2069,7 +2069,7 @@ static unsigned int get_rr_interval_rt(struct rq *rq, struct task_struct *task)
 }
 
 const struct sched_class rt_sched_class = {
-	.next			= &fair_sched_class,
+	.next			= &wrr_sched_class,
 	.enqueue_task		= enqueue_task_rt,
 	.dequeue_task		= dequeue_task_rt,
 	.yield_task		= yield_task_rt,
