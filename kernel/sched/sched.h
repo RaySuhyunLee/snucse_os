673febb3b946d53cb8c3902b84638d1517d9a84a
RaySuhyunLee <raysuhyunlee@gmail.com>
2017-05-03 16:16:03 +0900

    Add wrr_sched_class reference to header file

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1028,7 +1028,7 @@ extern const struct sched_class stop_sched_class;
 extern const struct sched_class rt_sched_class;
 extern const struct sched_class fair_sched_class;
 extern const struct sched_class idle_sched_class;
-
+extern const struct sched_class wrr_sched_class;
 
 #ifdef CONFIG_SMP
 
