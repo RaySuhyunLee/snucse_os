89ba7dfa256441deceb2c5312190c32f8cb1f3de
RaySuhyunLee <raysuhyunlee@gmail.com>
2017-05-05 22:06:15 +0900

    Implement sched.c. Error case test SUCCESS

--- kernel/sched.c
+++ kernel/sched.c
@@ -3,16 +3,42 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/uidgid.h>
+
+kuid_t root_uid = { 0 };
+
+#define IS_ROOT(uid) uid_eq((uid), root_uid)
+#define IS_CURR_ROOT() IS_ROOT(current_euid())
+
 /*
  *  * Set the SCHED_WRR weight of process, as identified by 'pid'.
  *   * If 'pid' is 0, set the weight for the calling process.
  *    * System call number 380.
  *     */
 int sys_sched_setweight(pid_t pid, int weight) {
-	printk(KERN_DEBUG "SET");
-	return 1;
+	struct task_struct *p;
+	
+	if (pid == 0) {
+		p = get_current();
+	} else {
+		p = find_task_by_vpid(pid);
+		if (!p) {
+			return -EINVAL;
+		}
 
+		if (!uid_eq(task_uid(p), current_uid()) 
+			&& !IS_CURR_ROOT()) {
+			return -EPERM;
+		}
+	}
 
+	if (weight > p->wrr.weight && !IS_CURR_ROOT()) {
+		return -EPERM;
+	}
+
+	// FIXME lock may be required.
+	p->wrr.weight = weight;
+	return 0;
 }
 /*
  *  * Obtain the SCHED_WRR weight of a process as identified by 'pid'.
@@ -20,6 +46,18 @@ int sys_sched_setweight(pid_t pid, int weight) {
  *    * System call number 381.
  *     */
 int sys_sched_getweight(pid_t pid){
+	struct task_struct *p;
+
  	printk(KERN_DEBUG "GET");
-	return 1;
+
+	if (pid == 0) {
+		p = get_current();
+	} else {
+		p = find_task_by_vpid(pid);
+		if (!p) {
+			return -EINVAL;
+		}
+	}
+
+	return p->wrr.weight;
 }
--- test/syscall_test.c
+++ test/syscall_test.c
@@ -6,11 +6,18 @@
 
 
 
-int main () {
-	int a = syscall(380,1,1);
-	int b = syscall(381,1);
+int main (int argc, char* argv[]) {
+	int pid = 0;
+	if (argc == 2) {
+		pid = atoi(argv[1]);
+	}
 
+	printf("testing with pid: %d\n", pid);
 
+	int a = syscall(380,pid,1);
+	int b = syscall(381,pid);
 
+	printf("sys_sched_setweight returned with %d\n", a);
+	printf("sys_sched_getweight returned with %d\n", b);
 	return 0;
 }
