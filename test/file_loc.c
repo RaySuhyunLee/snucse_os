f23dc7680d3fbd006e2225a54eeb62ecbfd28015
JaeDong Hwang <jdhwang730@gmail.com>
2017-05-29 22:28:16 +0900

    make file_loc.c

---
+++ test/file_loc.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<errno.h>
+
+struct gps_location {
+	int lat_integer;
+	int lat_fractional;
+	int lng_integer;
+	int lng_fractional;
+	int accuracy;
+};
+
+
+int main (int argc, char** argv) {
+	char *path = argv[1];
+	printf("%s\n",path);
+ 	struct gps_location* file_loc;
+	syscall(381,path, file_loc);
+	printf("https://www.google.co.kr/maps/place/");
+	printf("%d.%06d°N+%d.%06d°E\n",file_loc->lat_integer, file_loc->lat_fractional, file_loc->lng_integer, file_loc->lng_fractional);
+
+
+	return 0;
+
+}
+	
